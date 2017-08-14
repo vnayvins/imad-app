afba18b2abc6a42e4aecad706ea0e024ab5dd29a
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-14 15:27:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("sub-texxt");
 element.innerHTML="wohooooo....first app.....xcited a lot....";
+var img=document.getElementById("dragon") ;
+img.onclick = function() {
+    dragon.style.marginright="100px";
+    
+};
