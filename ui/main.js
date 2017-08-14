a088887f176ac88060fe7d0ee07c9aea11aedb97
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-14 15:34:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element=document.getElementById("sub-text");
 element.innerHTML="wohooooo....first app.....xcited a lot....";
 var img=document.getElementById("madi") ;
 img.onclick = function() {
-    img.style.marginright="100px";
+    img.style.marginRight="100px";
     
 };
