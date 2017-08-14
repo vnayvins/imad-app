411d2dfe31ced7cd45284bda209c678478eedece
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-14 17:17:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ element.innerHTML="wohooooo....first app.....xcited a lot....";
 var img=document.getElementById("madi") ;
 var marginLeft=0;
 function moveRight() {
-    marginLeft=marginLeft +10;
+    marginLeft=marginLeft +1;
     img.style.marginLeft=marginLeft+'px';
 }
 
 img.onclick = function() {
-   var interval=setInterval(moveRight,100);
+   var interval=setInterval(moveRight,50);
     };
