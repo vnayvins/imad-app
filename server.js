ea6797a523897a60b65d789235a9f1804b00668e
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-15 14:18:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ app.get('/page-3',function(req ,res) {
     
 });
 
+var counter=0;
+app.get('/counter',function(req,res) {
+    counter=counter+1;
+    res.send(counter.toString());
+    
+});
+
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
