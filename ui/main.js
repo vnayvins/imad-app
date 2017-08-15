f4d610adba698efa62fc078913e034d4aad4d24b
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-15 14:32:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,8 @@
-console.log('Loaded!');
-var element=document.getElementById("sub-text");
-element.innerHTML="wohooooo....first app.....xcited a lot....";
-var img=document.getElementById("madi") ;
-var marginLeft=0;
-function moveRight() {
-    marginLeft=marginLeft +1;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onclick = function() {
-   var interval=setInterval(moveRight,10);
-    };
+var button=document.getElementById("counter");
+var counter=0;
+button .onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=conter.toString();
+    
+};
