634eda810cdc8afe7046e4a253786dc833db450b
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-21 19:35:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config = {
+    user:'vinayvins11',
+    database:'vinayvins11',
+    host:'db_imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    };
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +17,24 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config)
+app.get('/counter',function(req,res) {
+    
+    pool.query('SELECT * FROM test',function(err,result) {
+        
+        if(err ) {
+            res.status(500).send(err.toString());
+             
+        }
+    else {
+        res.send(JSON.stringify(result));
+    }
+        
+    });
+
+});
+
 
 app.get('/page-1',function(req ,res) {
   res.sendFile(path.join(__dirname, 'ui', 'page-1.html'));
@@ -23,6 +50,13 @@ app.get('/page-3',function(req ,res) {
     res.sendFile(path.join(__dirname, 'ui', 'page-3.html'));
     
 });
+
+
+app.get('/test-db', function(req, res)  {
+    
+});
+
+
 
 var counter=0;
 app.get('/counter',function(req,res) {
