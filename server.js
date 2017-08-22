2b7188733cbe9f3293a912df5d252e013e5f07e9
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-22 11:20:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 function hash( input,salt ){
     
     var hashed=crypto.pbkdf2sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ['pbkdf' ,'10000' ,salt ,hashed.toString('hex')].join('$');
     
 }
 
