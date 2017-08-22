bb3933d3f33995c3890ef8fd9e10e4027074198e
vnayvins <30903303+vnayvins@users.noreply.github.com>
2017-08-22 11:55:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,47 @@ app.get('/counter',function(req,res) {
 
 });
 
+app.post('/login', function(req,res){
+    
+     var username=req.body.username;
+    var password=req.body.password;
+    
+    
+
+    pool.query('SELECT * from "user1" username= $1',[username] ,function(err,result) {
+        
+        if(err ) {
+            res.status(500).send(err.toString());
+             
+        }
+    else {
+        
+        if(result.rows.length===0) {
+            res.send(403).send('username/password is invalid');
+          }  else {
+           
+           var dbString=result.rows[0].password;
+           var salt=dbString.split('$').[2];
+           var hashedPassword=hash(password,salt);
+           if(hashedPassword===dbString) {
+               
+           }
+           res.send('credentials r correct');   
+          } else  {
+                          res.send(403).send('username/password is invalid');
+
+          }
+        
+    }
+    
+        
+    });
+});
+
+    
+});
+
+
 
 app.get('/page-1',function(req ,res) {
   res.sendFile(path.join(__dirname, 'ui', 'page-1.html'));
